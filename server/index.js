180e5a5f4b915a937a900f62c66f5942617393fa
Nick Hernandez <xnickhernandez@gmail.com>
2022-01-20 04:45:51 +0000

    Checking request body/params in backend

--- server/index.js
+++ server/index.js
@@ -18,7 +18,9 @@ app.use(jsonMiddleware);
 app.use(staticMiddleware);
 
 app.post("/api/newProject", (req, res, next ) => {
-  res.status(200).json({ message: "we made it!" })
+  console.log('params: ', req.params);
+  console.log('body: ', req.body);
+  res.status(200).json({ message: "we made it!" });
 });
 
 app.use(errorMiddleware);
