7fe3aa5bd63c8d2bcef8050bab6793495a133d98
Nick Hernandez <xnickhernandez@gmail.com>
2022-01-20 05:20:29 +0000

    Created database table for notes

--- database/schema.sql
+++ database/schema.sql
@@ -6,10 +6,22 @@ create schema "public";
 
 create table "users" (
   "id"  serial,
+  primary key ("id")
 );
 
 create table "projects" (
-  "id"  serial,
+  "id"     serial,
   "owner"  int not null,
   "title"  text not null,
+  primary key ("id")
+);
+
+create table "notes" (
+  "id"      serial,
+  "project" int not null,
+  "title"   text not null,
+  "data"    text not null,
+  "createdAt" timestamptz(6) not null default now(),
+  "lastEdittedAt" text not null,
+  primary key ("id")
 );
