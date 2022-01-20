13ce8e803d52aa944a111eda9999ab5d70e2005c
Nick Hernandez <xnickhernandez@gmail.com>
2022-01-20 07:17:22 +0000

    implemented useState and useEffect hooks to record user data and retrieve a jwt on load

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { parseToken, createToken } from './lib'
 import HeaderSection from './layouts/components/headerSection';
 
 const App = () => {
+  const [ user, setUser ] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const user = token ? parseToken(token) : createToken();
+
+  });
+
   return (
     <>
     <HeaderSection />
