ac0a4038eef32ffc39189cf45e7153b303c76afb
Nick Hernandez <xnickhernandez@gmail.com>
2022-02-12 20:08:08 +0000

    Created function to retrieve user projects #2

--- client/pages/thoughtBoard.jsx
+++ client/pages/thoughtBoard.jsx
@@ -4,13 +4,15 @@ import HeaderSection from '../layouts/components/headerSection';
 
 const ThoughtBoard = () => {
   const [ user, setUser ] = useState(null);
-  const [ project, setProject ] = useState(null);
+  const [ currentProject, setProject ] = useState(null);
+  const [ userProjects, setUserProjects ] = useState(null);
 
   useEffect(() => {
     const token = window.localStorage.getItem('token');
     if (token) {
       const userInfo = parseToken(token);
       setUser(userInfo);
+      getUserProjects(userInfo.id);
       return;
     }
     const fetchToken = async () => {
@@ -22,8 +24,18 @@ const ThoughtBoard = () => {
     fetchToken();
   }, []);
 
+  const getUserProjects = async userId => {
+    const response = await fetch(`/api/userProjects/${userId}`);
+    const projectData = await response.json();
+    setUserProjects(projectData);
+  };
+
+  console.log('user: ', user);
+  console.log('current proj: ', currentProject);
+  console.log('user projects: ', userProjects);
+
   return (
-    <HeaderSection user={user} setProject={setProject}/>
+    <HeaderSection user={user} setProject={setProject} projects={userProjects}/>
   );
 };
 
