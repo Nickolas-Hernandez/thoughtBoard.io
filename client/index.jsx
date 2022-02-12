64a07e2c66d5eb860f8f6ddae39ce668dd994088
Nick Hernandez <xnickhernandez@gmail.com>
2022-02-12 20:07:25 +0000

    File clean up

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM  from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './utils/themeConfig'
+import theme from './utils/themeConfig';
 import './styles/resets.css';
-
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
