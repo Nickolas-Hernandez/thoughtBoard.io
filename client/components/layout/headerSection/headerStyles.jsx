4caa9ccf32383be2584f96a95987866ea3b82ba7
Nick Hernandez <xnickhernandez@gmail.com>
2022-06-16 05:34:56 +0000

    update header styles

--- client/components/layout/headerSection/headerStyles.jsx
+++ client/components/layout/headerSection/headerStyles.jsx
@@ -5,14 +5,14 @@ const StyledHeader = styled(Box)({
   backgroundColor: '#2C2C31',
   color: '#FAF9F6',
   height: '90px',
-  padding: '.5rem 1rem',
+  padding: '.5rem 10px',
   display: 'flex',
   alignItems: 'flex-end',
   justifyContent: 'space-between'
 });
 
 const ProjectContainer = styled(Box)({
-  padding: '0 1rem',
+  padding: '0',
   display: 'flex',
   alignItems: 'flex-end'
 });
