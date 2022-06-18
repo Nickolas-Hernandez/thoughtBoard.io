ab2663e21628d52cabbc0cf1110689594065366c
Nick Hernandez <xnickhernandez@gmail.com>
2022-06-18 03:10:27 +0000

    add new note button

--- client/components/layout/sideBar/sideBar.jsx
+++ client/components/layout/sideBar/sideBar.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List, ListItem } from '@mui/material';
 import { StyledSideBar, StyledAddIcon, paperStyles } from './sideBarStyles';
 
 const SideBar = props => {
+  // create state to hold notes
+  const [ notes, setNotes ] = useState([]);
+
+  const createNewNote = () => {
+    // create new li for note and append to notes array
+    const noteModel = {
+      id: notes.length - 1, // fix this
+      title: '',
+      created: new Date(),
+      lastUpdate: new Date(),
+      data: ''
+    };
+  };
+
+  // map notes
+
   return (
-  <StyledSideBar variant="permanent" anchor="left" open='false' PaperProps={{ style: paperStyles }}>
+  <StyledSideBar variant="permanent" anchor="left" PaperProps={{ style: paperStyles }}>
     <List>
-      <ListItem>
-        <StyledAddIcon />
-      </ListItem>
+
     </List>
+    <StyledAddIcon onClick={createNewNote}/>
   </StyledSideBar>
   );
 };
