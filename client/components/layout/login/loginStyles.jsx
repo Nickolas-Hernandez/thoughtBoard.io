ba17e171f1ae604140b6dbe5382d957916ed3d4a
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-11-24 20:32:12 -0800

    add sign up feature

---
+++ client/components/layout/login/loginStyles.jsx
@@ -0,0 +1,36 @@
+import { TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const BackgroundImage = styled('div')(({ theme }) => ({
+  backgroundImage: 'url(assets/iewek-gnos-hhUx08PuYpc-unsplash.jpg)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  position: 'absolute',
+  width: '50%',
+  height: '100%',
+  right: 0,
+  top: 0,
+  [theme.breakpoints.down('md')]: {
+    width: '100%',
+    height: '50%'
+  }
+}));
+
+const StyledPaper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(4),
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  maxWidth: 400
+}));
+
+const StyledInputField = styled(TextField)(({ theme }) => ({
+  color: 'black'
+}));
+
+export {
+  BackgroundImage,
+  StyledPaper,
+  StyledInputField
+};
--- client/views/thoughtBoard.jsx
+++ client/views/thoughtBoard.jsx
@@ -1,37 +1,44 @@
 import React, { useEffect } from 'react';
-import { parseToken, createToken, useUser } from '../lib';
+import { parseToken, createToken, useUser, useAuth } from '../lib';
 import { getProjects } from '../services';
 import HeaderSection from '../components/layout/headerSection';
 import SideBar from '../components/layout/sideBar';
+import Login from '../components/layout/login';
 
 const ThoughtBoard = () => {
-  const userContext = useUser();
+  const { auth, login, logout } = useAuth();
+  // const userContext = useUser();
 
-  useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    const assignExistingUser = async token => {
-      const userInfo = parseToken(token);
-      userContext.setUser(userInfo);
-      const userProjects = await getProjects(userInfo.id);
-      userContext.setUserProjects(userProjects);
-    };
-    if (token) {
-      assignExistingUser(token);
-      return;
-    }
-    const fetchToken = async () => {
-      const newToken = await createToken();
-      const newUser = parseToken(newToken);
-      window.localStorage.setItem('token', newToken);
-      userContext.setUser(newUser);
-    };
-    fetchToken();
-  }, []);
+  // useEffect(() => {
+  //   const token = window.localStorage.getItem('token');
+  //   const assignExistingUser = async token => {
+  //     const userInfo = parseToken(token);
+  //     userContext.setUser(userInfo);
+  //     const userProjects = await getProjects(userInfo.id);
+  //     userContext.setUserProjects(userProjects);
+  //   };
+  //   if (token) {
+  //     assignExistingUser(token);
+  //     return;
+  //   }
+  //   const fetchToken = async () => {
+  //     const newToken = await createToken();
+  //     const newUser = parseToken(newToken);
+  //     window.localStorage.setItem('token', newToken);
+  //     userContext.setUser(newUser);
+  //   };
+  //   fetchToken();
+  // }, []);
+
+  if (!auth.isLoggedIn) {
+    return <Login onLogin={login} />;
+  }
 
   return (
     <>
       <HeaderSection />
       <SideBar />
+      <button onClick={logout}>Logout</button>
     </>
   );
 };
