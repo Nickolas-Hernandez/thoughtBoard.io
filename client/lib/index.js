bb8578796f76151c8ec9cfbb1ec38a090056e9a9
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-11-24 20:32:12 -0800

    add sign up feature

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,3 +1,4 @@
 export { default as parseToken } from './parseToken';
 export { default as createToken } from './createToken';
 export { UserProvider, useUser } from './UserContext';
+export { AuthProvider, useAuth } from './UserAuth';
